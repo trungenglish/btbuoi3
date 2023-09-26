55d86167616af96c7f73a8c490fe9a0eb57b95e8
trungenglish <nguyentrung23232323@gmail.com>
2023-09-26 17:02:33 +0700

    new

---
+++ bth3ajs/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="bth3ajs.MvcApplication" Language="C#" %>
---
+++ bth3ajs/th/script.js
@@ -0,0 +1,14 @@
+﻿function printNumbers() {
+    const numberInput = document.getElementById('numberInput').value;
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = ''; 
+    if (!isNaN(numberInput) && parseInt(numberInput) > 0) {
+        for (let i = 1; i <= parseInt(numberInput); i++) {
+            const span = document.createElement('span');
+            span.innerText = i + ' ';
+            outputDiv.appendChild(span);
+        }
+    } else {
+        outputDiv.innerText = 'Vui lòng nhập một số nguyên dương.';
+    }
+}
